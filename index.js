4926cf225a90517db16a4f235688696ad0afacca
Dmitry-Dev <i6169696@gmail.com>
2023-11-21 15:03:30 +0300

    big fix

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ const token = process.env.TOKEN;
 const bot = new TelegramBot(token, {polling: true});
 
 // Обработка команды /start
-bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, 'Добро пожаловать в наш чат!');
+bot.onText(/\/start/, async (msg) => {
+    await bot.sendSticker(msg.chat.id, 'https://tlgrm.eu/_/stickers/8a1/9aa/8a19aab4-98c0-37cb-a3d4-491cb94d7e12/192/1.webp');
+    await bot.sendMessage(msg.chat.id, 'Добро пожаловать в наш чат!');
 });
 
 // Обработка текстовых сообщений
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    console.log(msg)
-    if (msg.text.includes('moba')) {
-        (async () => {
+bot.on('message', async (msg) => {
+    try{
+        const chatId = msg.chat.id;
+        console.log(msg)
+        if (msg.text.includes('moba')) {
             const browser = await puppeteer.launch({
                 headless: "new"
             })
@@ -32,14 +33,24 @@ bot.on('message', (msg) => {
             let data = await page.evaluate(selector => {
                 return document.querySelector(selector).innerText;
             }, '.price_value');
+            let name = await page.evaluate(selector => {
+                return document.querySelector(selector).innerText
+            }, '#pagetitle');
             data = data.replace(' ', '');
             const outDataBy = ((data / 77 + 1.5) * 3).toFixed()
             const outDataUSD = ((data / 77 + 1.5)).toFixed()
-            await bot.sendMessage(chatId, `Цена с доставкой: ${outDataUSD} USD или ${outDataBy} Бел`);
+            await bot.sendMessage(chatId, `Ты спрашивал: ${name} \nЦена с доставкой: ${outDataUSD} USD или ${outDataBy} Бел`);
             await page.close()
             await browser.close()
-        })()
+            return
+
+        }
+
     }
-});
+    catch (e){
+        console.log(e)
+    }
+    return bot.sendMessage(chatId, 'Я пока работаю только с сайтом moba.ru')
+})
 
 
